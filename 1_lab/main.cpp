11a9fed698a191db87a3b91a0b7d0394aac34d1b
aksenianets <aksenianets@gmail.com>
2024-10-06 19:44:21 +0300

    complete 1 lab

--- 1_lab/main.cpp
+++ 1_lab/main.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <iomanip>
-#include <climits>
-#include <cmath>
 
 using namespace std;
 
@@ -24,20 +22,35 @@ int main()
     cout << "signed long long     " << sizeof(signed long long) << "       " << LLONG_MIN << " - " << LLONG_MAX << endl;
     cout << "unsigned long long   " << sizeof(unsigned long long) << "       " << 0 << " - " << ULLONG_MAX << endl;
 
+    cout << "bool                 " << sizeof(bool) << "       " << 0 << " / " << 1 << endl;
+
+    cout << "float                " << sizeof(float) << "       " << FLT_MIN << " - " << FLT_MAX << endl;
+    cout << "double               " << sizeof(double) << "       " << DBL_MIN << " - " << DBL_MAX << endl;
+    cout << "long double          " << sizeof(long double) << "       " << LDBL_MIN << " - " << LDBL_MAX << endl;
+
     cout << "\nExemples:" << endl;
 
     char c = 65;
     cout << "char " << c << " " << char(c + 1) << endl;
 
     // для переменных типов short, int, long операции будут выполнятся одинаково
-    long long i = 125;
-    cout << "int " << i << " " << i << endl;
+    long long l1 = 125'452'789'463;
+    long long l2 = 124'896'542'142;
+    cout << "long long " << l1 + l2 << endl;
 
     // если выйти за пределы значений переменной, то она применет своё минимальное/максимальное 
     short s1 = 32767;
     short s2 = -32768;
     cout << "unsigned short " << s1 << " " << short(s1 + 1) << endl;
     cout << "unsigned short " << s2 << " " << short(s2 - 1) << endl;
+
+    double d1 = 4.56;
+    double d2 = 4552.635657;
+    cout << "double " << d1 + d2 << endl;
+
+    bool b1 = 1;
+    bool b2 = false;
+    cout << "bool " << b1 || b2;
 
     return 0;
 }
